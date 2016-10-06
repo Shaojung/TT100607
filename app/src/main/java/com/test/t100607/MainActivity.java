962068d7d477d2c718f5b2d7abe40eb0296e9bbd
Shaojung <shaojung@gmail.com>
2016-10-06 15:05:49 +0800

    Add two button.

--- app/src/main/java/com/test/t100607/MainActivity.java
+++ app/src/main/java/com/test/t100607/MainActivity.java
@@ -1,5 +1,6 @@
 package com.test.t100607;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +18,18 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("這是對話框");
         builder.setMessage("這是對話框的內容");
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
         builder.show();
     }
 }
