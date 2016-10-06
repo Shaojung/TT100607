ac93c02168601a989fbcd0c1614780744c4152a9
Shaojung <shaojung@gmail.com>
2016-10-06 15:37:47 +0800

    Add EditText. and Toast.

--- app/src/main/java/com/test/t100607/MainActivity.java
+++ app/src/main/java/com/test/t100607/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,8 +44,23 @@ public class MainActivity extends AppCompatActivity {
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("這是對話框");
-        EditText ed = new EditText(MainActivity.this);
+        final EditText ed = new EditText(MainActivity.this);
         builder.setView(ed);
+
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, ed.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
         builder.show();
     }
 
